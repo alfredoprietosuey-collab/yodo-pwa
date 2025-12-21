2c68f3e46837538fe9a5a1608bb2d135d131d83d
alfredoprietosuey-collab <alfredoprietosuey@gmail.com>
2025-12-21 18:43:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-function go(url) {
-  window.location.href = url;
+const AFF = {
+  AMAZON: "https://amzn.to/3L46fn2",
+  EL_CORTE_INGLES: "https://tidd.ly/4aEdfSb",
+  MEDIAMARKT: "https://www.mediamarkt.es",
+  DECATHLON: "https://www.decathlon.es"
+};
+
+function go(key) {
+  window.location.href = AFF[key];
 }
